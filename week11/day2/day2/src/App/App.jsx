bb73cf80842cbb420247ab304b83bbe1f1d4c010
Denzil-l <Denzil.l@yandex.ru>
2023-07-25 20:02:05 +0300

    hw

--- week11/day2/day2/src/App/App.jsx
+++ week11/day2/day2/src/App/App.jsx
@@ -1,11 +1,35 @@
 import { useState } from 'react'
-import SunRise from '../Components/WeatherAPI';
+
 
 
 const App = () => {
+  const [languages, setLanguages] = useState([
+    { name: "Php", votes: 0 },
+    { name: "Python", votes: 0 },
+    { name: "JavaSript", votes: 0 },
+    { name: "Java", votes: 0 }
+  ])
+  const increaseVote = (index) => {
+    setLanguages(languages => languages.map((l, i) => {
+      if (i == index) {
+        l.votes++
+        return l
+      } else {
+        return l
+      }
+    }))
+  }
   return (
     <div>
-      <SunRise />
+      <ul>
+        {languages.map((element, index) => (
+          <li key={index}>
+            {element.votes}
+            {element.name}
+            <button onClick={() => increaseVote(index)}>Click Here</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
